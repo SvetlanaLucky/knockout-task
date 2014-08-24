b81184f84f9e6da07f6af52fbd64f84034003990
SvetlanaLucky <svetlana.slight@gmail.com>
2014-08-24 19:04:23 +0300

    create structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-knockout-task
-=============
+#My demo-project
+
---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Slight
+ * Date: 24.08.14
+ * Time: 19:00
+ * To change this template use File | Settings | File Templates.
+ */
