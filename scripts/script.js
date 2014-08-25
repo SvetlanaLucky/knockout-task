2825d2a6d71918172fb7e61eaf81d605def4224b
SvetlanaLucky <svetlana.slight@gmail.com>
2014-08-25 07:03:54 +0300

    functionality ready, some styles needed

--- scripts/script.js
+++ scripts/script.js
@@ -7,32 +7,32 @@
  */
 var literatureList='[' +
     '{'+
-    '"index":"1",' +
+    '"index":"1. ",' +
     '"title":"О компании QAP INT",' +
     '"iconFile":"assets/images/filetype_pdf.png"' +
     '},' +
     '{'+
-    '"index":"2",' +
+    '"index":"2. ",' +
     '"title":"Преимущества использования CLM-инструмента для Вашего бизнеса",' +
     '"iconFile":"assets/images/filetype_pdf.png"' +
     '},' +
     '{'+
-    '"index":"3",' +
+    '"index":"3. ",' +
     '"title":"Перспективы сотрудничества с QAP INT",' +
     '"iconFile":"assets/images/filetype_pdf.png"' +
     '},' +
     '{'+
-    '"index":"4",' +
+    '"index":"4. ",' +
     '"title":"Описание решения для разработки HTML5 презентаций",' +
     '"iconFile":"assets/images/filetype_pdf.png"' +
     '},' +
     '{'+
-    '"index":"5",' +
+    '"index":"5. ",' +
     '"title":"Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com (русский)",' +
     '"iconFile":"assets/images/filetype_pdf.png"' +
     '},' +
     '{'+
-    '"index":"6",' +
+    '"index":"6. ",' +
     '"title":"Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com (english)",' +
     '"iconFile":"assets/images/filetype_pdf.png"' +
     '}'+
@@ -47,20 +47,13 @@ function AppViewModel(){
     self.topic=ko.observable("topic");
     self.post=ko.observable("text");
     self.attachment=ko.observableArray();
-
-//    self.addAttachment=function($index){
-//        self.attachment.push($index);
-
-//        console.log(isChecked());
-//        console.log($index);
-//    };
-
-
-
-
-
-
+    self.openPopUp=ko.observable();
+    self.open=function(){
+        self.openPopUp("visible");
+    };
+    self.close=function(){
+        self.openPopUp("");
+    };
 }
-
 
 ko.applyBindings(new AppViewModel());
